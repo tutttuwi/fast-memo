5e1d1d5b5039c0433b6f2f813bced77dd6c9ddfe
t-tsutsui <t.tsutsui9117@gmail.com>
2023-06-23 01:26:26 +0900

    initial commit. copy template

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# fast-memo
+# Fast Memo
+
+## Overview
+
+FastMemo allows you to quickly access note on browser popup page.
+
+## Demo
+
+- TBD
+
+## Build
+
+```cmd
+rem Build for production
+pnpm build
+
+rem Build for dev
+pnpm dev:watch
+
+```
+
+## Lisence
+
+MIT
+
+## Author
+
+t-tsutsui
+
+
---
+++ src/background/main.ts
@@ -0,0 +1,55 @@
+import { onMessage, sendMessage } from 'webext-bridge';
+import type { Tabs } from 'webextension-polyfill';
+import { getMessage } from '~/background/i18n';
+// import { Storage } from '~/logic/storage';
+// const storage = new Storage();
+
+// only on dev mode
+if (import.meta.hot) {
+  // @ts-expect-error for background HMR
+  import('/@vite/client');
+  // load latest content script
+  import('./contentScriptHMR');
+}
+
+browser.runtime.onInstalled.addListener(async () => {
+  // eslint-disable-next-line no-console
+  console.log('Extension installed');
+});
+
+let previousTabId = 0;
+
+// communication example: send previous tab title from background page
+// see shim.d.ts for type declaration
+browser.tabs.onActivated.addListener(async ({ tabId }) => {
+  if (!previousTabId) {
+    previousTabId = tabId;
+    return;
+  }
+
+  let tab: Tabs.Tab;
+
+  try {
+    tab = await browser.tabs.get(previousTabId);
+    previousTabId = tabId;
+  } catch {
+    return;
+  }
+
+  // eslint-disable-next-line no-console
+  console.log('previous tab', tab);
+  sendMessage('tab-prev', { title: tab.title }, { context: 'content-script', tabId });
+});
+
+onMessage('get-current-tab', async () => {
+  try {
+    const tab = await browser.tabs.get(previousTabId);
+    return {
+      title: tab?.title,
+    };
+  } catch {
+    return {
+      title: undefined,
+    };
+  }
+});
